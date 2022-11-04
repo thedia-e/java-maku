d7390deb9ba68b3bdbaae4c0bae07513ebacfa7f
edanur <115102893+Edanur-s@users.noreply.github.com>
2022-11-04 19:51:53 +0300

    Add files via upload

---
+++ Hundred.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author edaza
+ */
+public class Hundred {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int number = 0 ;
+       for (int i=0; i<=100; i++)
+       {
+           if (number<100) 
+           {
+            number = number + 10 ;
+           System.out.println(number);
+           }
+                     
+        }
+    }
+    
+}

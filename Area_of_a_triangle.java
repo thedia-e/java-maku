645db6cc625311dda9e1df1bd8a63cec91b97d6c
edanur <115102893+Edanur-s@users.noreply.github.com>
2022-11-04 20:02:57 +0300

    Add files via upload

---
+++ Area_of_a_triangle.java
@@ -0,0 +1,26 @@
+/*
+
+ */
+
+/**
+ *
+ * @author edaza
+ */
+import java.util.Scanner;
+public class Area_of_a_triangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in) ;
+        System.out.println("Please enter a number value for the side of this triangle");
+        int x = input.nextInt();
+        System.out.println("Please enter a number for the height value of that side");
+        int h = input.nextInt();
+        int area = (h*x)/2 ;
+        System.out.println("Area of this triangle is : " +area);
+        
+    }
+    
+}
